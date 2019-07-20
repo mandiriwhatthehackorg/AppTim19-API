082a2e5dc9ae0ec85c78b9f4b1b558c2b10f6e5a
I Wayan Dharmana <dharmanacyber@Gmail.com>
2019-07-21 04:27:12 +0700

    set token

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
 <?php
+header('Content-Type: application/json');
 
-echo "okk its work :)";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL,"http://apigateway.mandiriwhatthehack.com/rest/pub/apigateway/jwt/getJsonWebToken?app_id=c3590064-bfef-4784-b87b-7459f02f8aec");
+//curl_setopt($ch, CURLOPT_POST, 1);
+//curl_setopt($ch, CURLOPT_POSTFIELDS,$vars);  //Post Fields
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+$headers = [
+    'Accept: application/json',
+    'Authorization: Basic ZWNmYTI4MDMtMDQwZC00NDRjLWIyODgtOWM3ODY4MWUxMmE1OmVlMGY4YTJiLTBmNmUtNDQyMS1iNzEzLWNlNzhlODg5NjA2MA'
+];
+
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+$server_output = curl_exec ($ch);
+
+curl_close ($ch);
+
+print  $server_output ;
 
 ?>
